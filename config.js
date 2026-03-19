deba0bc0440234b9e5a2205a6275771b806c0978
MomentariyModder <82979292+MomentariyModder@users.noreply.github.com>
2026-03-19 20:08:05 +0300

    2

---
+++ config.js
@@ -0,0 +1,21 @@
+/*!
+ * MomentariyModder Website 8.0.0 by MomentariyModder
+ * The source code is available on GitHub!
+ */
+
+var server_name = "NCS 3.5";
+var server_ip = "mc.nclcs.net";
+var server_port = "25565";
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+
+vote("Mod", "img/projects/momentariymodderapplications/momentariycore2.png", "#ma");
+vote("Mod", "img/projects/perodiumcraft/perodiumcraft2.png", "#perodiumcraft");
+vote("Mod", "img/projects/spongebobsquarepentsreload/spongebobsquepents.png", "#ssreload");
+vote("Mod", "img/projects/night_vision_helmets/night_vision_helmets_avatar.png", "#nvh");
+vote("Mod", "img/projects/created_music_discs/created_music_discs_avatar.png", "#cmd");
+vote("Mod", "img/projects/created_spawn_eggs/created_spawn_eggs_avatar.png", "#cse");
+vote("Mod", "img/projects/window_doors_and_trapdoors/window_doors_and_trapdoors_avatar.png", "#wdt");
+vote("Mod", "img/coming_soon_project.png", "#cs_p");
